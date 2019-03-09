0bf866653e9243edbb636eb400edc6912ac7a169
PermanentPortionX <1627982@students.wits.ac.za>
2019-03-09 09:31:43 +0200

    app icon changed

--- app/src/main/java/com/triplec/paul/phinda/skhumbuzo/mulisa/witssrcconnect/LogInActivity.java
+++ app/src/main/java/com/triplec/paul/phinda/skhumbuzo/mulisa/witssrcconnect/LogInActivity.java
@@ -3,11 +3,11 @@ package com.triplec.paul.phinda.skhumbuzo.mulisa.witssrcconnect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
     }
 }
