63f5438bfacaab4c01bb7730c9e2d2184c35c0c4
PermanentPortionX <1627982@students.wits.ac.za>
2019-03-28 19:26:04 +0200

    created layout for student to view src activities

--- app/src/main/java/com/wits/witssrcconnect/fragments/StudentViewSrcActivitiesFragment.java
+++ app/src/main/java/com/wits/witssrcconnect/fragments/StudentViewSrcActivitiesFragment.java
@@ -1,4 +1,24 @@
 package com.wits.witssrcconnect.fragments;
 
-public class StudentViewSrcActivitiesFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wits.witssrcconnect.R;
+
+
+public class StudentViewSrcActivitiesFragment extends Fragment {
+
+    private static View v;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_student_view_src_activities, container, false);
+
+        return v;
+    }
 }
