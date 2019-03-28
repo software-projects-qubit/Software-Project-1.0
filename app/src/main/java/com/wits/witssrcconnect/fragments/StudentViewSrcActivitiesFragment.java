7c75dca2c1d9af9e218826ee35a9df92c7eec1de
PermanentPortionX <1627982@students.wits.ac.za>
2019-03-28 19:33:29 +0200

    combined StudentViewSrcActivitiesFragment with fragment_student_view_src_activities.xml

--- app/src/main/java/com/wits/witssrcconnect/fragments/StudentViewSrcActivitiesFragment.java
+++ app/src/main/java/com/wits/witssrcconnect/fragments/StudentViewSrcActivitiesFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.wits.witssrcconnect.R;
+import com.wits.witssrcconnect.managers.SrcActivityManager;
+import com.wits.witssrcconnect.managers.UiManager;
+
+import org.json.JSONArray;
 
 
 public class StudentViewSrcActivitiesFragment extends Fragment {
@@ -24,5 +29,13 @@ public class StudentViewSrcActivitiesFragment extends Fragment {
 
     private void init() {
         if (v == null) return;
+        SrcActivityManager.fetchAllActivities(getContext(), null);
+        SwipeRefreshLayout pullToRefresh = v.findViewById(R.id.pullToRefreshSrcActivitiesStudent);
+        pullToRefresh.setOnRefreshListener(() -> SrcActivityManager.fetchAllActivities(getContext(), pullToRefresh));
+    }
+
+    public static void init(JSONArray activities) {
+        if (v == null) return;
+        UiManager.populateWithSrcActivities(v.findViewById(R.id.student_src_activities_view), activities);
     }
 }
