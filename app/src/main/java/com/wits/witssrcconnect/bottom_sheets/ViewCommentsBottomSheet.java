5caae81464ee0c0cdbcaf9d603db7c302e2b0378
PermanentPortionX <1627982@students.wits.ac.za>
2019-03-29 00:01:44 +0200

    created view comment bottom sheet, this bottom sheet will pop up when a user presses view comment

---
+++ app/src/main/java/com/wits/witssrcconnect/bottom_sheets/ViewCommentsBottomSheet.java
@@ -0,0 +1,6 @@
+package com.wits.witssrcconnect.bottom_sheets;
+
+import android.support.design.widget.BottomSheetDialogFragment;
+
+public class ViewCommentsBottomSheet extends BottomSheetDialogFragment {
+}
