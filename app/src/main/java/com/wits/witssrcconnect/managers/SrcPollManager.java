d6aa38b2df766ba1f6054ed1eb3fdbd7e4ba7fdf
MbusoMakitla <1699140@students.wits.ac.za>
2019-03-29 11:09:50 +0200

    implemented server communicator to talk to the database and post poll

--- app/src/main/java/com/wits/witssrcconnect/managers/SrcPollManager.java
+++ app/src/main/java/com/wits/witssrcconnect/managers/SrcPollManager.java
@@ -3,9 +3,21 @@ package com.wits.witssrcconnect.managers;
 import android.content.ContentValues;
 
 import com.wits.witssrcconnect.activities.SrcPostPollActivity;
+import com.wits.witssrcconnect.services.ServerCommunicator;
+import com.wits.witssrcconnect.utils.ServerUtils;
 
 public class SrcPollManager {
     public static void postPoll(ContentValues cv, SrcPostPollActivity srcPostPollActivity) {
+        new ServerCommunicator(cv) {
+            @Override
+            protected void onPreExecute() {
 
+            }
+
+            @Override
+            protected void onPostExecute(String output) {
+
+            }
+        }.execute(ServerUtils.POLL_LINK);
     }
 }
