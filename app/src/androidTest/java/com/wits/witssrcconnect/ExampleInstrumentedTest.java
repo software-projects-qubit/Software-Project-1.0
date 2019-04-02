c3cc21872e88e291ca41a26996f679c35461a07e
PermanentPortionX <mulisa0998@gmail.com>
2019-04-02 14:57:42 +0200

    updated ExampleInstrumentTest.java

--- app/src/androidTest/java/com/wits/witssrcconnect/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wits/witssrcconnect/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.triplec.paul.phinda.skhumbuzo.mulisa.witssrcconnect", appContext.getPackageName());
+        assertEquals("com.wits.witssrcconnect", appContext.getPackageName());
     }
 }
