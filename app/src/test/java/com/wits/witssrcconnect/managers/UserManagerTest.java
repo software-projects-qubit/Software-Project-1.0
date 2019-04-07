262f2627565ffb9e104db9add0be39655e9db1dc
MbusoMakitla <1699140@students.wits.ac.za>
2019-04-07 20:55:56 +0200

    added in user manager

---
+++ app/src/test/java/com/wits/witssrcconnect/managers/UserManagerTest.java
@@ -0,0 +1,16 @@
+package com.wits.witssrcconnect.managers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserManagerTest {
+
+    @Test
+    public void setUserLoggedIn() {
+    }
+
+    @Test
+    public void getCurrentlyLoggedInUsername() {
+    }
+}
