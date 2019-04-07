7b15540f7f3560b4729ac4a41f9195b33134e74b
MbusoMakitla <1699140@students.wits.ac.za>
2019-04-07 21:52:48 +0200

    tests ofr sha1 string

---
+++ app/src/test/java/com/wits/witssrcconnect/managers/SecurityManagerTest.java
@@ -0,0 +1,12 @@
+package com.wits.witssrcconnect.managers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SecurityManagerTest {
+
+    @Test
+    public void SHA1() {
+    }
+}
