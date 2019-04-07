bf7324adbba4161f294d0ffddc5db9369a309531
MbusoMakitla <1699140@students.wits.ac.za>
2019-04-07 19:47:37 +0200

    added tests

---
+++ app/src/test/java/com/wits/witssrcconnect/activities/LogInActivityTest.java
@@ -0,0 +1,17 @@
+package com.wits.witssrcconnect.activities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LogInActivityTest {
+
+    @Test
+    public void onCreate() {
+
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+}
