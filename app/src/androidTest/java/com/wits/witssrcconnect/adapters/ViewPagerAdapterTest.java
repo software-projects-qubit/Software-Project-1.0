ec276343dbc4d6909287295397db837999b569f3
PermanentPortionX <1627982@students.wits.ac.za>
2019-05-03 00:07:19 +0200

    exclusion test

---
+++ app/src/androidTest/java/com/wits/witssrcconnect/adapters/ViewPagerAdapterTest.java
@@ -0,0 +1,37 @@
+package com.wits.witssrcconnect.adapters;
+
+import android.annotation.SuppressLint;
+import android.support.test.InstrumentationRegistry;
+import android.support.v4.app.FragmentManager;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@SuppressLint("IgnoreWithoutReason")
+public class ViewPagerAdapterTest {
+
+
+
+    @Ignore
+    @Test
+    public void getItem() {
+    }
+
+    @Ignore
+    @Test
+    public void addFragment() {
+    }
+
+    @Ignore
+    @Test
+    public void getCount() {
+    }
+
+    @Ignore
+    @Test
+    public void getPageTitle() {
+    }
+}
