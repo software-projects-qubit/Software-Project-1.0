1a5c75be2ebe91c970b4cda7e6d5ee784991feca
PermanentPortionX <1627982@students.wits.ac.za>
2019-05-04 23:27:35 +0200

    testing new codecov approach

--- app/src/androidTest/java/com/wits/witssrcconnect/adapters/ViewPagerAdapterTest.java
+++ app/src/androidTest/java/com/wits/witssrcconnect/adapters/ViewPagerAdapterTest.java
@@ -1,37 +1,53 @@
 package com.wits.witssrcconnect.adapters;
 
-import android.annotation.SuppressLint;
-import android.support.test.InstrumentationRegistry;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-@SuppressLint("IgnoreWithoutReason")
 public class ViewPagerAdapterTest {
 
+    private static ViewPagerAdapter v = null;
+    private static Fragment testFrag;
 
+    @BeforeClass
+    public static void setUp(){
+        FragmentManager fm = Mockito.mock(FragmentManager.class);
+        testFrag = new Fragment();
 
-    @Ignore
+        v = new ViewPagerAdapter(fm);
+        v.addFragment(new Fragment(), "Test1");
+        v.addFragment(testFrag, "");
+        v.addFragment(new Fragment(), "");
+        v.addFragment(new Fragment(), "");
+    }
+
     @Test
     public void getItem() {
+        Fragment f = v.getItem(1);
+        assertEquals(testFrag, f);
     }
 
-    @Ignore
     @Test
     public void addFragment() {
+        v.addFragment(new Fragment(), "");
+        v.addFragment(new Fragment(), "");
+        v.addFragment(new Fragment(), "");
+        assertEquals(7, v.getCount());
     }
 
-    @Ignore
     @Test
     public void getCount() {
+        v.addFragment(new Fragment(), "");
+        assertEquals(8, v.getCount());
     }
 
-    @Ignore
     @Test
     public void getPageTitle() {
+        assertEquals("Test1", v.getPageTitle(0));
     }
 }
