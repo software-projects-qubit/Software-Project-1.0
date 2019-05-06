8bd023e01d133ae6f94fd95e00a37d008769e553
ThabisoMac <1757861@students.wits.ac.za>
2019-05-06 02:10:19 +0200

    implemented initial test case for button click

--- app/src/androidTest/java/com/wits/witssrcconnect/activities/LogInActivityTest.java
+++ app/src/androidTest/java/com/wits/witssrcconnect/activities/LogInActivityTest.java
@@ -2,10 +2,15 @@ package com.wits.witssrcconnect.activities;
 
 import android.support.test.rule.ActivityTestRule;
 
+import com.wits.witssrcconnect.R;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 public class LogInActivityTest {
@@ -22,7 +27,7 @@ public class LogInActivityTest {
 
     @Test
     public void pressStudent(){
-        //onViewWithId(R.id.)
+        onView(withId(R.id.appCompatImageView2)).perform(click());
     }
 
 }
