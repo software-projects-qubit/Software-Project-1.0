60669a50ffb7d6c72dc81c33b35121c7c52ff8e2
Skhu47 <1608194@students.wits.ac.za>
2019-05-11 19:25:15 +0200

    added button press test cases

--- app/src/androidTest/java/com/wits/witssrcconnect/activities/LogInActivityTest.java
+++ app/src/androidTest/java/com/wits/witssrcconnect/activities/LogInActivityTest.java
@@ -1,8 +1,64 @@
 package com.wits.witssrcconnect.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.wits.witssrcconnect.R;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class LogInActivityTest {
 
+    private static Context c = InstrumentationRegistry.getTargetContext();
+
+    @Rule
+    public ActivityTestRule<LogInActivity> activityTestRule = new ActivityTestRule<>(LogInActivity.class);
+
+    @Test
+    public void studentPressed(){
+        onView(withId(R.id.appCompatImageView2)).perform(click());
+    }
+
+    @Test
+    public void srcMemberPressed(){
+        onView(withId(R.id.member_icon_holder)).perform(click());
+    }
+
+    @Test
+    public void buttonPressedThenBackPressed(){
+        try {
+            runOnUiThread(() ->{
+                activityTestRule.getActivity().findViewById(R.id.appCompatImageView2).performClick();
+                activityTestRule.getActivity().onBackPressed();
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
+
+    @Test
+    public void backPressed(){
+
+        try {
+            runOnUiThread(() -> activityTestRule.getActivity().onBackPressed());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    }
 
 }
