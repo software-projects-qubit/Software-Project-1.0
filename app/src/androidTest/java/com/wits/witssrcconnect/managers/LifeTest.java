31cc1215c9afb167eda3413899602395e31fef1a
Skhu47 <1608194@students.wits.ac.za>
2019-05-11 17:20:31 +0200

    wrote test cases that start all the activities

---
+++ app/src/androidTest/java/com/wits/witssrcconnect/managers/LifeTest.java
@@ -0,0 +1,78 @@
+package com.wits.witssrcconnect.managers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.wits.witssrcconnect.activities.LogInActivity;
+import com.wits.witssrcconnect.activities.SrcMemberActivity;
+import com.wits.witssrcconnect.activities.SrcPostActivityActivity;
+import com.wits.witssrcconnect.activities.SrcPostPollActivity;
+import com.wits.witssrcconnect.activities.StudentActivity;
+import com.wits.witssrcconnect.utils.ServerUtils;
+import com.wits.witssrcconnect.utils.UserUtils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
+@RunWith(AndroidJUnit4.class)
+public class LifeTest {
+
+    private Context c = InstrumentationRegistry.getTargetContext();
+
+    @Test
+    public void startLoginActivity(){
+        getInstrumentation().runOnMainSync(() ->{
+            Intent i = new Intent(c, LogInActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            c.startActivity(i);
+        });
+    }
+
+    @Test
+    public void startSrcMemberActivity(){
+        int user = UserUtils.SRC_MEMBER;
+        UserManager.initUserManager(c);
+        UserManager.setUserLoggedIn(user, "srcpresident");
+
+        getInstrumentation().runOnMainSync(() ->{
+            Intent i = new Intent(c, SrcMemberActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            c.startActivity(i);
+        });
+    }
+
+    @Test
+    public void startStudentActivity(){
+        int user = UserUtils.STUDENT;
+        UserManager.initUserManager(c);
+        UserManager.setUserLoggedIn(user, "1627982");
+
+        getInstrumentation().runOnMainSync(() ->{
+            Intent i = new Intent(c, StudentActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            c.startActivity(i);
+        });
+    }
+
+    @Test
+    public void startPostActivity(){
+        getInstrumentation().runOnMainSync(() ->{
+            Intent i = new Intent(c, SrcPostActivityActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            c.startActivity(i);
+        });
+    }
+
+    @Test
+    public void startPostPollActivity(){
+        getInstrumentation().runOnMainSync(() ->{
+            Intent i = new Intent(c, SrcPostPollActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            c.startActivity(i);
+        });
+    }
+}
