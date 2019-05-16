3ceb8645ba0c426dded74dd582dd56a585d6b74a
PermanentPortionX <1627982@students.wits.ac.za>
2019-05-16 22:09:44 +0200

    fixed incorrectly coded test cases

--- app/src/androidTest/java/com/wits/witssrcconnect/managers/UiManagerTest.java
+++ app/src/androidTest/java/com/wits/witssrcconnect/managers/UiManagerTest.java
@@ -81,81 +81,128 @@ public class UiManagerTest {
 
     @Test
     public void sendComment(){
-        TextInputEditText comment = new TextInputEditText(c);
-        int[] a = new int[1];
-        a[0] = ServerUtils.ANONYMOUS_COMMENT_ON;
+        try {
+            runOnUiThread(()->{
+                TextInputEditText comment = new TextInputEditText(c);
+                int[] a = new int[1];
+                a[0] = ServerUtils.ANONYMOUS_COMMENT_ON;
 
-        UiManager.sendComment(comment, a, anyInt());
-        comment.setText("test text");
-        UiManager.sendComment(comment, a, anyInt());
+                UiManager.sendComment(comment, a, anyInt());
+                comment.setText("test text");
+                UiManager.sendComment(comment, a, anyInt());
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     @Test
     public void openComments(){
-        FragmentManager fragmentManager = Mockito.mock(FragmentManager.class);
-        UiManager.openComments(anyInt(), anyString(), anyString(), fragmentManager);
+        try {
+            runOnUiThread(()->{
+                FragmentManager fragmentManager = Mockito.mock(FragmentManager.class);
+                UiManager.openComments(anyInt(), anyString(), anyString(), fragmentManager);
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
     }
 
     @Test
     public void deleteActivity(){
-        LinearLayout holder = new LinearLayout(c);
-        holder.setOrientation(LinearLayout.VERTICAL);
-        View activityItemView = View.inflate(holder.getContext(), R.layout.item_src_activity_card, null);
-        holder.addView(activityItemView);
-        UiManager.deleteActivity(anyInt(), activityItemView, holder);
+        try {
+            runOnUiThread(()->{
+                LinearLayout holder = new LinearLayout(c);
+                holder.setOrientation(LinearLayout.VERTICAL);
+                View activityItemView = View.inflate(holder.getContext(), R.layout.item_src_activity_card, null);
+                holder.addView(activityItemView);
+                UiManager.deleteActivity(anyInt(), activityItemView, holder);
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
     }
 
     @Test
     public void handleAnonymity(){
-        int[] a = new int[1];
-        UiManager.handleAnonymity(true, a, c);
-        UiManager.handleAnonymity(false, a, c);
+        try {
+            runOnUiThread(()->{
+                int[] a = new int[1];
+                UiManager.handleAnonymity(true, a, c);
+                UiManager.handleAnonymity(false, a, c);
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
     }
 
     @Test
     public void deleteItem(){
-        UiManager.deleteItem(new ContentValues(), new LinearLayout(c), new View(c), anyString());
+        try {
+            runOnUiThread(()->{
+                UiManager.deleteItem(new ContentValues(), new LinearLayout(c), new View(c), anyString());
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
     }
 
     @Test
     public void handleDeleteItemFeedback(){
-        LinearLayout l = new LinearLayout(c);
-        l.setOrientation(LinearLayout.VERTICAL);
+        try {
+            runOnUiThread(()->{
+                LinearLayout l = new LinearLayout(c);
+                l.setOrientation(LinearLayout.VERTICAL);
 
-        View v = new View(c);
-        l.addView(v);
+                View v = new View(c);
+                l.addView(v);
 
-        UiManager.handleDeleteItemFeedback(ServerUtils.FAILED, l, v);
-        UiManager.handleDeleteItemFeedback(ServerUtils.SUCCESS, l, v);
+                UiManager.handleDeleteItemFeedback(ServerUtils.FAILED, l, v);
+                UiManager.handleDeleteItemFeedback(ServerUtils.SUCCESS, l, v);
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
 
     }
 
     @Test
     public void populateWithPolls(){
-        JSONArray testJsonArray = new JSONArray();
-        JSONObject testJsonObject = new JSONObject();
+
         try {
-            testJsonObject.put(ServerUtils.POLL_TITLE, anyString());
-            testJsonObject.put(ServerUtils.SRC_USERNAME, anyString());
-            testJsonObject.put(ServerUtils.POLL_DATE, anyString());
-            testJsonObject.put(ServerUtils.POLL_TIME, anyString());
-            testJsonObject.put(ServerUtils.POLL_DESC, anyString());
-            String options = "opt1~opt2~opt3";
-            testJsonObject.put(ServerUtils.POLL_CHOICE, options);
-            testJsonObject.put(ServerUtils.POLL_TYPE, anyInt());
+            runOnUiThread(()->{
+                JSONArray testJsonArray = new JSONArray();
+                JSONObject testJsonObject = new JSONObject();
+                try {
+                    testJsonObject.put(ServerUtils.POLL_TITLE, anyString());
+                    testJsonObject.put(ServerUtils.SRC_USERNAME, anyString());
+                    testJsonObject.put(ServerUtils.POLL_DATE, anyString());
+                    testJsonObject.put(ServerUtils.POLL_TIME, anyString());
+                    testJsonObject.put(ServerUtils.POLL_DESC, anyString());
+                    String options = "opt1~opt2~opt3";
+                    testJsonObject.put(ServerUtils.POLL_CHOICE, options);
+                    testJsonObject.put(ServerUtils.POLL_TYPE, anyInt());
 
-            testJsonArray.put(testJsonObject);
-            testJsonArray.put(testJsonObject);
-            testJsonArray.put(testJsonObject);
+                    testJsonArray.put(testJsonObject);
+                    testJsonArray.put(testJsonObject);
+                    testJsonArray.put(testJsonObject);
 
-            LinearLayout holder = new LinearLayout(c);
-            holder.setOrientation(LinearLayout.VERTICAL);
-            FragmentManager fragmentManager = Mockito.mock(FragmentManager.class);
-            UiManager.populateWithPolls(holder, testJsonArray, fragmentManager);
-        } catch (JSONException e) {
-            e.printStackTrace();
+                    LinearLayout holder = new LinearLayout(c);
+                    holder.setOrientation(LinearLayout.VERTICAL);
+                    FragmentManager fragmentManager = Mockito.mock(FragmentManager.class);
+                    UiManager.populateWithPolls(holder, testJsonArray, fragmentManager);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
         }
-
     }
 
     @Test
