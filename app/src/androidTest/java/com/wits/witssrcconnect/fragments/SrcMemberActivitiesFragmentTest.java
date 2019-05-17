9b8c32aecc63dc46e49ffec9c1d42f7d0e9ea990
PermanentPortionX <1627982@students.wits.ac.za>
2019-05-17 08:32:05 +0200

    modified test cases

--- app/src/androidTest/java/com/wits/witssrcconnect/fragments/SrcMemberActivitiesFragmentTest.java
+++ app/src/androidTest/java/com/wits/witssrcconnect/fragments/SrcMemberActivitiesFragmentTest.java
@@ -6,14 +6,22 @@ import com.wits.witssrcconnect.R;
 
 import org.junit.Test;
 
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 import static org.junit.Assert.*;
 
 public class SrcMemberActivitiesFragmentTest {
 
     @Test
     public void buttonClicksTest(){
-        SrcMemberActivitiesFragment.v.findViewById(R.id.FAB_src_activity_add).performClick();
-        SwipeRefreshLayout sr = SrcMemberActivitiesFragment.v.findViewById(R.id.src_activities_swipe_refresh_layout);
-        sr.scrollTo(2, 2);
+        try {
+            runOnUiThread(()->{
+                SrcMemberActivitiesFragment.v.findViewById(R.id.FAB_src_activity_add).performClick();
+                SwipeRefreshLayout sr = SrcMemberActivitiesFragment.v.findViewById(R.id.src_activities_swipe_refresh_layout);
+                sr.scrollTo(2, 2);
+            });
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
     }
 }
